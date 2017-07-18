1de2742c8a433a0bad21b9d19718c0d7b8908a8d
Dan Brown <slightlyoffbeat@gmail.com>
2017-07-18 15:06:55 -0700

    remove prefetch

--- components/Header.js
+++ components/Header.js
@@ -6,8 +6,8 @@ const baseUrl = variables.prodUrl;
 const Header = () => (
   <header>
     <nav>
-      <Link prefetch href={`${baseUrl}/`}><a>Home</a></Link> |
-      <Link prefetch href={`${baseUrl}/about`}><a> About</a></Link>
+      <Link href={`${baseUrl}/`}><a>Home</a></Link> |
+      <Link href={`${baseUrl}/about`}><a> About</a></Link>
     </nav>
   </header>
 );
