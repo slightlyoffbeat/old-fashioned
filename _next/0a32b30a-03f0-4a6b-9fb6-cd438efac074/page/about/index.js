057cce2358e5f4ee3ea58972997e7c73eb885c3e
Dan Brown <slightlyoffbeat@gmail.com>
2017-07-18 06:46:05 -0700

    Updates

---
+++ _next/0a32b30a-03f0-4a6b-9fb6-cd438efac074/page/about/index.js
@@ -0,0 +1,6 @@
+
+          window.__NEXT_REGISTER_PAGE('/about', function() {
+            var comp = module.exports=webpackJsonp([5],{338:function(e,t,u){e.exports=u(339)},339:function(e,t,u){"use strict";function n(e){return e&&e.__esModule?e:{default:e}}Object.defineProperty(t,"__esModule",{value:!0});var l=u(7),o=n(l),a=u(168),d=n(a);t.default=function(){return o.default.createElement(d.default,{title:"Old Fashioned | About"},o.default.createElement("div",null,"About Page"))}}},[338]);
+            return { page: comp.default }
+          })
+        
---
+++ _next/0a32b30a-03f0-4a6b-9fb6-cd438efac074/page/index.js
@@ -0,0 +1,6 @@
+
+          window.__NEXT_REGISTER_PAGE('/', function() {
+            var comp = module.exports=webpackJsonp([4],{354:function(e,t,l){e.exports=l(355)},355:function(e,t,l){"use strict";function u(e){return e&&e.__esModule?e:{default:e}}Object.defineProperty(t,"__esModule",{value:!0});var a=l(7),r=u(a),n=l(168),d=u(n),i=l(174);t.default=function(){return r.default.createElement(d.default,{title:"Old Fashioned | Home"},r.default.createElement("div",null,"Whiskey & Bitters & Sugar & Ice"),r.default.createElement("img",{src:i.prodUrl+"/static/img/sample.jpg",alt:"sample"}))}}},[354]);
+            return { page: comp.default }
+          })
+        
