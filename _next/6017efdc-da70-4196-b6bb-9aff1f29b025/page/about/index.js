edbdc4d2b42c7bd420985765153ec81dac414be0
Dan Brown <slightlyoffbeat@gmail.com>
2017-07-18 01:34:55 -0700

    Updates

--- _next/6017efdc-da70-4196-b6bb-9aff1f29b025/page/about/index.js
+++
@@ -1,6 +0,0 @@
-
-          window.__NEXT_REGISTER_PAGE('/about', function() {
-            var comp = module.exports=webpackJsonp([5],{336:function(e,t,u){e.exports=u(337)},337:function(e,t,u){"use strict";function n(e){return e&&e.__esModule?e:{default:e}}Object.defineProperty(t,"__esModule",{value:!0});var l=u(7),r=n(l),a=u(167),o=n(a);t.default=function(){return r.default.createElement(o.default,{title:"Whiskey. Sugar. Bitters. Ice."},r.default.createElement("div",null,"About Page."))}}},[336]);
-            return { page: comp.default }
-          })
-        
--- _next/6017efdc-da70-4196-b6bb-9aff1f29b025/page/index.js
+++
@@ -1,6 +0,0 @@
-
-          window.__NEXT_REGISTER_PAGE('/', function() {
-            var comp = module.exports=webpackJsonp([4],{341:function(e,t,u){e.exports=u(342)},342:function(e,t,u){"use strict";function l(e){return e&&e.__esModule?e:{default:e}}Object.defineProperty(t,"__esModule",{value:!0});var n=u(7),r=l(n),o=u(167),a=l(o);t.default=function(){return r.default.createElement(a.default,{title:"Whiskey. Sugar. Bitters. Ice."},r.default.createElement("div",null,"Hello World."))}}},[341]);
-            return { page: comp.default }
-          })
-        
